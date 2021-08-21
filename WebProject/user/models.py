f292fd0481a28114b6515f415de36b408aa75711
renjunbin <renjunbin@126.com>
2021-08-21 13:25:43 +0800

    配置环境

--- WebProject/user/models.py
+++ WebProject/user/models.py
@@ -1,4 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
 
+class User(models.Model):
+    img = models.CharField(max_length=200, blank=True)
+    name = models.CharField(max_length=200, unique=True)
+    pwd = models.CharField(max_length=200)
+    email = models.EmailField(max_length=200)
+    createtime = models.DateField(auto_now=True)
+    updatetime = models.DateField(auto_now_add=True)
+    is_merchant = models.BooleanField(default=False)
+
+    def __str__(self):
+        return u"User:%s's name" % self.name
