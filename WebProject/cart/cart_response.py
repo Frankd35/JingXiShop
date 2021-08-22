d4a048382011b9496304042ae1649aafb4343f71
CloudAndMist <854947489@qq.com>
2021-08-22 11:58:14 +0800

    cart后端推送数据字段增加

--- WebProject/cart/cart_response.py
+++ WebProject/cart/cart_response.py
@@ -5,7 +5,8 @@ from user import models as user_model
 from cart import models as cart_model
 
 class ShowGoods:
-    def __init__(self, name, img, num, price, shopName):
+    def __init__(self, gid, name, img, num, price, shopName):
+        self.gid = gid
         self.name = name
         self.img = img
         self.num = num
@@ -22,7 +23,7 @@ def dealRequest(user_id):
         tempShopName = '哈哈哈店铺'
         tempName = tempGoods.name
         tempImg = tempGoods.img
-        hhh= ShowGoods(tempName,tempImg,i.goods_num,i.goods_price,tempShopName)
+        hhh = ShowGoods(i.goods_id, tempName, tempImg, i.goods_num, i.goods_price, tempShopName)
         goodsList.append(hhh)
         print("name:%s,img:%s,num:%d,price:%f,shopName:%s"%(hhh.name, hhh.img, hhh.num, hhh.price, hhh.shopName))
 
