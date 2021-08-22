9bfad57530af93ac77c9aa650f531f7b9a0e9a07
CloudAndMist <854947489@qq.com>
2021-08-22 15:49:10 +0800

    cart后端逻辑基本完善

--- WebProject/cart/cart_response.py
+++ WebProject/cart/cart_response.py
@@ -6,7 +6,7 @@ from cart import models as cart_model
 
 
 class ShowGoods:
-    def __init__(self, gid, name, img, num, price, shopName):
+    def __init__(self, gid, name, img, num, price, shopName, tttprice=0):
         self.gid = gid
         self.name = name
         self.img = img
@@ -14,6 +14,12 @@ class ShowGoods:
         self.price = price
         self.shopName = shopName
         self.sum = price * num
+        self.tttprice = tttprice
+
+class ListInt:
+    def __init__(self,goodsList,total_price):
+        self.goodsList = goodsList
+        self.total_price = total_price
 
 
 def dealRequest(user_id, flag, gid, isChosen):
@@ -31,19 +37,28 @@ def dealRequest(user_id, flag, gid, isChosen):
             cart_model.Cart.objects.filter(user_id=user_id, goods_id=gid).update(is_chosen=1)
         else:  # 该商品未被选中
             cart_model.Cart.objects.filter(user_id=user_id, goods_id=gid).update(is_chosen=0)
+    elif flag == 2:  # 增加
+        ori_num = cart_model.Cart.objects.filter(ser_id=user_id, goods_id=gid)[0].goods_num
+        cart_model.Cart.objects.filter(user_id=user_id, goods_id=gid).update(goods_num=(ori_num+1))
+    elif flag == 3: # 减少
+        ori_num = cart_model.Cart.objects.filter(ser_id=user_id, goods_id=gid)[0].goods_num
+        if ori_num > 0:
+            cart_model.Cart.objects.filter(user_id=user_id, goods_id=gid).update(goods_num=(ori_num - 1))
+    elif flag == 4: # 删除
+        cart_model.Cart.objects.filter(user_id=user_id, goods_id=gid).first().delete()
     for i in tempList:
         tempGoods = goods_model.Goods.objects.filter(id=i.goods_id)[0]
         # 从数据库获取店铺信息
         tempShopName = '哈哈哈店铺'
         tempName = tempGoods.name
         tempImg = tempGoods.img
-        hhh = ShowGoods(i.goods_id, tempName, tempImg, i.goods_num, i.goods_price, tempShopName)
         if i.is_chosen != 0:
-            total_price += hhh.sum
+            total_price += i.goods_price * i.goods_num
+        hhh = ShowGoods(i.goods_id, tempName, tempImg, i.goods_num, i.goods_price, tempShopName, total_price)
         goodsList.append(hhh)
         print("name:%s,img:%s,num:%d,price:%f,shopName:%s" % (hhh.name, hhh.img, hhh.num, hhh.price, hhh.shopName))
 
-    return goodsList, total_price
+    return goodsList
 
 
 def deleteGoods(uid, gid):
--- WebProject/cart/views.py
+++ WebProject/cart/views.py
@@ -1,3 +1,5 @@
+import json
+
 from django.shortcuts import render
 
 from django.shortcuts import render
@@ -32,24 +34,34 @@ def cart_view(request):
     m = request.method
     # GET请求， 加载页面
     if m == 'GET':
-        goodsList, total_price = dealRequest(user_id, 0, gid, 0)
-        return render(request, 'shopcar2.html', {'goodsList': goodsList, 'user': user, 'total_price':total_price})
+        goodsList = dealRequest(user_id, 0, gid, 0)
+        if len(goodsList) != 0:
+            total_price = goodsList[len(goodsList) - 1].tttprice
+        else:
+            total_price = 0
+        print('Total_price:'+str(total_price))
+        return render(request, 'shopcar2.html', {'goodsList': goodsList, 'user': user, 'total_price': total_price})
     # POST请求， 根据请求中flag的值判断操作类型
     else:
-        gid = int(request.POST.get('gid', ''))
-        flag = request.POST.get('flag', '')
-        isChosen = bool(request.POST.get('checked', ''))
+        data = request.body.decode("utf-8")
+        json_data = json.load(data)
+        gid = int(json_data.get('gid', -1))
+        flag = json_data.get('flag', '')
+        isChosen = bool(json_data.get('checked', False))
         goodsList = []
         total_price = 0
         if flag == 'check':  # check逻辑
-            goodsList, total_price = dealRequest(user_id, 1, gid, isChosen)
+            goodsList = dealRequest(user_id, 1, gid, isChosen)
         elif flag == 'add':  # add 逻辑
-            goodsList, total_price = dealRequest(user_id, 2, gid, 0)
+            goodsList = dealRequest(user_id, 2, gid, 0)
         elif flag == 'sub':  # sub 逻辑
-            goodsList, total_price = dealRequest(user_id, 3, gid, 0)
+            goodsList = dealRequest(user_id, 3, gid, 0)
         elif flag == 'delete':  # delete 逻辑
-            goodsList, total_price = dealRequest(user_id, 4, gid, 0)
-
+            goodsList = dealRequest(user_id, 4, gid, 0)
+        if len(goodsList) != 0:
+            total_price = goodsList[len(goodsList) - 1].tttprice
+        else:
+            total_price = 0
         return render(request, 'shopcar2.html', {'goodsList': goodsList, 'user': user, 'total_price':total_price})
 
 
