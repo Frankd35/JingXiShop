9ead5f3bff31a3e6d0713d14e95e33173457fbc8
renjunbin <renjunbin@126.com>
2021-08-23 11:09:31 +0800

    auto

--- WebProject/index/views.py
+++ WebProject/index/views.py
@@ -2,6 +2,8 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 
 # Create your views here.
+from sympy.codegen.ast import none
+
 from user.models import User
 
 
@@ -12,7 +14,11 @@ def index_view(request):
 def index_template_view(request):
     usr_id = int(request.session.get('uid', -1))
     isLogin = usr_id != -1
+
     if not isLogin:
         render(request, 'index_template.html', {'isLogin': isLogin})
-    user = User.objects.get(id=usr_id)
+    try:
+        user = User.objects.get(id=usr_id)
+    except:
+        user = none
     return render(request, 'index_template.html', {'isLogin': isLogin, 'user': user})
