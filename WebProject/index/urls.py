87165f232e07d3404eeff235ae287882210e0d1a
renjunbin <renjunbin@126.com>
2021-08-23 09:49:40 +0800

    auto

--- WebProject/index/urls.py
+++ WebProject/index/urls.py
@@ -1,8 +1,8 @@
-#coding=utf-8
+# coding=utf-8
 
 from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$',views.index_view),
+    url(r'^$', views.index_view),
 ]
--- WebProject/index/views.py
+++ WebProject/index/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 # Create your views here.
 def index_view(request):
     return render(request, 'index.html')
+
+
+def index_template_view(request):
+    usr_id = int(request.session.get('uid', -1))
+    isLogin = usr_id != -1
+    return render(request, 'index_template.html', {'isLogin': isLogin})
