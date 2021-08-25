7e4933d6313895bcaa019093176768179e7e5d92
CloudAndMist <854947489@qq.com>
2021-08-25 10:33:40 +0800

    更新购物车后端逻辑，更新全局css样式

--- WebProject/cart/cart_response.py
+++ WebProject/cart/cart_response.py
@@ -6,7 +6,7 @@ from cart import models as cart_model
 
 
 class ShowGoods:
-    def __init__(self, gid, name, img, num, price, shopName, tttprice=0):
+    def __init__(self, gid, name, img, num, price, shopName, max_num, tttprice=0):
         self.gid = gid
         self.name = name
         self.img = img
@@ -14,6 +14,7 @@ class ShowGoods:
         self.price = price
         self.shopName = shopName
         self.sum = price * num
+        self.max_num = max_num
         self.tttprice = tttprice
 
 
@@ -33,7 +34,7 @@ def dealRequest(user_id, flag, gid, isChosen):
     print("flag=" + str(flag))
     if flag == 0:
         # 每次进入页面（GET 请求）重置is_chosen为0
-        cart_model.Cart.objects.filter(user_id=user_id).update(is_chosen=0, goods_num=1)
+        cart_model.Cart.objects.filter(user_id=user_id).update(is_chosen=0)
         print("GET请求重置")
     elif flag == 1:  # 选中 or 不选中
         if isChosen:  # 该商品被选中
@@ -69,6 +70,13 @@ def dealRequest(user_id, flag, gid, isChosen):
             cart_model.Cart.objects.filter(user_id=user_id).update(is_chosen=0)
             print("取消全选")
         return []
+    elif flag == 6: # 更新购物车商品数量 -> 此时isChosen表征购物车商品数量
+        if isChosen < 0:
+            print("没有取到num值")
+            return []
+        cart_model.Cart.objects.filter(user_id=user_id, goods_id=gid).update(goods_num=isChosen)
+        print("购物车商品数量更新")
+        return []
     for i in tempList:
         tempGoods = goods_model.Goods.objects.filter(id=i.goods_id).first()
         tempShop = user_model.User.objects.filter(id=i.shop_id).first()
@@ -80,7 +88,7 @@ def dealRequest(user_id, flag, gid, isChosen):
         tempPrice = tempGoods.price
         if i.is_chosen != 0:
             total_price += i.goods_price * i.goods_num
-        hhh = ShowGoods(i.goods_id, tempName, tempImg, i.goods_num, tempPrice, tempShopName, total_price)
+        hhh = ShowGoods(i.goods_id, tempName, tempImg, i.goods_num, tempPrice, tempShopName, tempGoods.number, total_price)
         goodsList.append(hhh)
         print("name:%s,img:%s,num:%d,price:%f,shopName:%s" % (hhh.name, hhh.img, hhh.num, hhh.price, hhh.shopName))
 
