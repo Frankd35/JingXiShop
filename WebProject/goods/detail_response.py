e72fc386322d5f0d343cb91048721d5f83723080
CloudAndMist <854947489@qq.com>
2021-08-26 10:59:44 +0800

    商品评论功能前后端连通，下版本处理ajax成功回调函数逻辑

--- WebProject/goods/detail_response.py
+++ WebProject/goods/detail_response.py
@@ -78,3 +78,10 @@ def addCollect(uid, gid):
     if cart_model.Favorite.objects.filter(user_id=uid, goods_id=gid).count() < 1:
         cart_model.Favorite.objects.create(user_id=uid, goods_id=gid, goods_img=tempGoods.img)
     return None
+
+
+def editComment(uid, gid, text, mark):
+    if goods_model.Comment.objects.filter(user_id=uid, goods_id=gid).count() > 0:
+        return 0
+    goods_model.Comment.objects.create(user_id=uid, goods_id=gid, text=text, mark=mark)
+    return 1
